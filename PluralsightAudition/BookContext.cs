c58e75bc4acc8dedc4be421b7fe5349bdc5be0fd
Peter Mbanugo <chuksgerrard@yahoo.com>
2020-01-28 10:54:18 +0100

    add constructor

--- PluralsightAudition/BookContext.cs
+++ PluralsightAudition/BookContext.cs
@@ -4,6 +4,9 @@ namespace PluralsightAudition
 {
     public class BookContext : DbContext
     {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
         public DbSet<Book> Books { get; set; }
     }
 }
